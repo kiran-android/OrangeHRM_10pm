8b3a81c7099809cd43aba9ae6c907a4ed06de7d5
DELL <DELL@DESKTOP-11QTGE5>
2024-07-17 12:45:03 +0530

    flight code added

---
+++ FlightsBook/src/com/ghmc/DemoGitTest.java
@@ -0,0 +1,13 @@
+package com.ghmc;
+
+public class DemoGitTest {
+
+	public static void main(String[] args) {
+		
+		int x=10;
+		int j=x+20;
+	System.out.println("Finally git done");
+    System.out.println("The sum of the numbers are" +j);
+	}
+
+}
